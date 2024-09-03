6bfbfc010068ce37d1249470b00881b25b8f0b58
Nikhil Gupta <77400458+Nikhil-251@users.noreply.github.com>
2024-09-03 13:46:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Welcome to Python Flask World v5.0 from cicd'
+    return 'Welcome to Python Flask World v1.0 from cicd on the Cloud Run'
 
 
 if __name__ == '__main__':
